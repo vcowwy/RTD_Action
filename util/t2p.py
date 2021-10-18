73b771c2b3404947c3fdd656d3b9e3306f44df70
陈婉玉 <cwy@bupt.edu.cn>
2021-10-18 19:03:27 +0800

    new t2p

--- util/t2p.py
+++ util/t2p.py
@@ -1,4 +1,5 @@
 import paddle
+import numpy as np
 
 
 class DistributedSampler(paddle.io.DistributedBatchSampler):
@@ -63,3 +64,93 @@ class DataLoader(paddle.io.DataLoader):
             worker_init_fn=worker_init_fn)
         if sampler is not None:
             self.batch_sampler.sampler = sampler
+
+
+class Linear(paddle.nn.Linear):
+    def __init__(self,
+                 in_features,
+                 out_features,
+                 bias=True):
+        uniform_bound = np.sqrt(1.0/in_features)
+        weight_attr = paddle.nn.initializer.Uniform(-uniform_bound, uniform_bound)
+        if not bias:
+            bias_attr = False
+        else:
+            bias_attr = paddle.nn.initializer.Uniform(-uniform_bound, uniform_bound)
+                #paddle.ParamAttr(name="bias")
+        super(Linear, self).__init__(in_features,
+                                     out_features,
+                                     weight_attr=weight_attr,
+                                     bias_attr=bias_attr)
+
+
+class Conv1d(paddle.nn.Conv1D):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 groups=1,
+                 bias=True,
+                 padding_mode='zeros'):
+        uniform_bound = np.sqrt(1.0 / in_channels)
+        weight_attr = paddle.nn.initializer.Uniform(-uniform_bound, uniform_bound)
+        if not bias:
+            bias_attr = False
+        else:
+            bias_attr = paddle.nn.initializer.Uniform(-uniform_bound, uniform_bound)
+        #bias_attr = None
+        #if not bias:
+        #    bias_attr = False
+        #else:
+        #    bias_attr =paddle.nn.initializer.MSRAInitializer() # fluid.initializer.ConstantInitializer(value=0)
+
+        super(Conv1d, self).__init__(in_channels=in_channels,
+                                     out_channels=out_channels,
+                                     kernel_size=kernel_size,
+                                     stride=stride,
+                                     padding=padding,
+                                     dilation=dilation,
+                                     groups=groups,
+                                     padding_mode=padding_mode,
+                                     weight_attr=weight_attr,
+                                     bias_attr=bias_attr,
+                                     data_format='NCL')
+
+
+class Conv2d(paddle.nn.Conv2D):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 groups=1,
+                 bias=True,
+                 padding_mode='zeros'):
+        uniform_bound = np.sqrt(1.0 / in_channels)
+        weight_attr = paddle.nn.initializer.Uniform(-uniform_bound, uniform_bound)
+        if not bias:
+            bias_attr = False
+        else:
+            bias_attr = paddle.nn.initializer.Uniform(-uniform_bound, uniform_bound)
+        # bias_attr = None
+        # if not bias:
+        #    bias_attr = False
+        # else:
+        #    bias_attr =paddle.nn.initializer.MSRAInitializer() # fluid.initializer.ConstantInitializer(value=0)
+
+        super(Conv2d, self).__init__(in_channels=in_channels,
+                                     out_channels=out_channels,
+                                     kernel_size=kernel_size,
+                                     stride=stride,
+                                     padding=padding,
+                                     dilation=dilation,
+                                     groups=groups,
+                                     padding_mode=padding_mode,
+                                     weight_attr=weight_attr,
+                                     bias_attr=bias_attr,
+                                     data_format='NCHW')
